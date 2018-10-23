38f978b78a274b12ac19704350ce21e37ce98408
Pickachu Chen <pickachu1006@gmail.com>
2018-10-23 16:41:23 +0800

    Update 2018-10-23T08:41:21.833Z

--- js/all.js
+++ js/all.js
@@ -3,7 +3,7 @@
 var place = document.getElementById('zone');
 var hotplace = document.querySelector('.hotplace');
 var title = document.querySelector('.place-name');
-var detail = document.querySelector('.place-detail');
+var detail = document.querySelector('.place-detail .row');
 var str = ""; //selector資料
 
 var zoneData = ["", "內門區", "大樹區", "小港區", "六龜區", "仁武區", "左營區", "田寮區", "永安區", "甲仙區", "鼓山區", "杉林區", "那瑪夏", "岡山區", "高雄市", "前鎮區", "茂林區", "茄萣區", "梓官區", "旗津區", "桃源區", "楠梓區", "前金區", "鳳山區", "美濃區", "苓雅區", "新興區"];
@@ -59,7 +59,8 @@ function getData() {
 
     for (var _i = 0; _i < len; _i++) {
       if (a[_i].Zone == str) {
-        detail.innerHTML += '<li><img class="top" src="' + a[_i].Picture1 + '"><div class="top-text"><div class="big">' + a[_i].Name + '</div><div class="small">' + a[_i].Zone + '</div></div></div><div class="bottom"><div class="icon"> <img src="images/clock.png" alt=""><span>' + a[_i].Opentime + '</span></div><div class="icon"><img src="images/pin.png" alt=""><span>' + a[_i].Add + '</span></div><div class="icon"><div class="phone"><img src="images/phone.png" alt=""><span>' + a[_i].Tel + '</span></div><div class="tag"><img src="images/tag.png" alt=""><span>' + a[_i].Ticketinfo + '</span></div></div></div></li>';
+        // detail.innerHTML+='<li><img class="top" src="'+a[i].Picture1+'"><div class="top-text"><div class="big">'+a[i].Name+'</div><div class="small">'+a[i].Zone+'</div></div></div><div class="bottom"><div class="icon"> <img src="images/clock.png" alt=""><span>'+a[i].Opentime+'</span></div><div class="icon"><img src="images/pin.png" alt=""><span>'+a[i].Add+'</span></div><div class="icon"><div class="phone"><img src="images/phone.png" alt=""><span>'+a[i].Tel+'</span></div><div class="tag"><img src="images/tag.png" alt=""><span>'+a[i].Ticketinfo+'</span></div></div></div></li>';
+        detail.innerHTML += "\n                <div class=\"col-md-6 my-3\">\n                 <div class=\"card\">\n                    <img src=\"".concat(a[_i].Picture1, "\" alt=\"Card image cap\" class=\"card-img-top\">\n                        <div class=\"top-text d-flex justify-content-between px-3 w-100\">\n                            <div class=\"big\">").concat(a[_i].Name, "</div>\n                            <div class=\"small\">").concat(a[_i].Zone, "</div>\n                        </div>\n                    <div class=\"card-body\">\n                        <div class=\"card-text\">\n                            <img src=\"images/clock.png\" alt=\"\" class=\"mr-2\">\n                            <span>").concat(a[_i].Opentime, "</span>\n                        </div>\n                        <div class=\"card-text my-1\">\n                            <img src=\"images/pin.png\" alt=\"\" class=\"mr-2\">\n                            <span>").concat(a[_i].Add, "</span>\n                        </div>\n                        <div class=\"card-text d-flex justify-content-between w-100\">\n                            <div class=\"phone\">\n                                <img src=\"images/phone.png\" alt=\"\" class=\"mr-2\">\n                                <span>").concat(a[_i].Tel, "8</span>\n                        </div>\n                        <div class=\"tag\">\n                            <img src=\"images/tag.png\" alt=\"\" class=\"mr-2\">\n                            <span>").concat(a[_i].Ticketinfo, "</span>\n                        </div>\n                        </div>\n                        </div>\n                    </div>\n                </div>\n                ");
       }
     }
   };
